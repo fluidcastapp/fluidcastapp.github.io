9a629591a2855c26d2cbf4b83592db4c43a7af33
jbishop <jbishop@protonmail.com>
2026-07-26 14:07:42 +0100

    Deploy FluidCast 1.4.0 web build

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1303570691" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2329761028" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
