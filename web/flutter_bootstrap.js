4b23d28f4e4fe42357b95372ac5fa7d73f863cd0
Joseph Bishop <support@fluidcastapp.com>
2026-08-23 09:56:36 +0100

    web player hotfix

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1082566269" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4002280640" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
